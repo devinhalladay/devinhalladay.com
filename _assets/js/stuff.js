cfecd6ff3185a43fd7ff00a20daeac1b60d8553c
Devin Halladay <devin@devinhalladay.com>
2013-11-05 12:01:33 -0500

    Added share links

--- _assets/js/stuff.js
+++ _assets/js/stuff.js
@@ -4,6 +4,7 @@
 //= require vendor/jquery.fitvids
 //= require vendor/parsley.min
 //= require vendor/parsley.extend
+//= require vendor/kudos
 
 $(function() {
   var nav = responsiveNav(".nav-collapse");
@@ -13,4 +14,18 @@ $(".video-pusher").fitVids();
 
 $(".post-sharing-button").on("click", function() {
     $(".sharing-menu").toggleClass("IsOpen")
+});
+
+function r(f){/in/.test(document.readyState)?setTimeout('r('+f+')',9):f()}
+
+r(function() {
+  /*
+   * Create Kudos Please widget
+   */
+  new KudosPlease({
+    el : '.kudos--default',
+    duration : 1500,
+    status : {
+    }
+  });
 });
--- _plugins/read-time.rb
+++ _plugins/read-time.rb
@@ -3,7 +3,7 @@ module ReadTimeFilter
   def readtime(input)
 
     charcount = 4.5
-    wpm = 180
+    wpm       = 180
 
     rt = (input.to_f/charcount/wpm).round
     rt = 1 if rt < 1
