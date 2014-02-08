0b4cc3bda1b557cc4a14fdb0e30bff474f981cea
Devin Halladay <devin@devinhalladay.com>
2014-02-08 15:24:05 -0500

    Removed fitvids

--- _assets/js/stuff.js
+++ _assets/js/stuff.js
@@ -1,27 +1,9 @@
 //= require vendor/responsive-nav.min
 //= require vendor/ss-standard
 //= require vendor/wufoo
-//= require vendor/jquery.fitvids
 //= require vendor/parsley.min
 //= require vendor/parsley.extend
 
 $(function() {
   var nav = responsiveNav(".nav-collapse");
 });
-
-$(".video-pusher").fitVids();
-
-/* $(".post-sharing-button").on("click", function() {
-    $(".sharing-menu").toggleClass("IsOpen")
-});
-
-function r(f){/in/.test(document.readyState)?setTimeout('r('+f+')',9):f()}
-
-r(function() {
-  new KudosPlease({
-    el : '.kudos--default',
-    duration : 1500,
-    status : {
-    }
-  });
-}); */
