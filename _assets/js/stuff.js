3a9abb11c562945c35299c5cf4ac217ebad3f39c
Devin Halladay <devin@devinhalladay.com>
2014-02-10 19:45:32 -0500

    Adding instantclick.js. It's awesome!

--- _assets/js/stuff.js
+++ _assets/js/stuff.js
@@ -3,6 +3,7 @@
 //= require vendor/wufoo
 //= require vendor/parsley.min
 //= require vendor/parsley.extend
+//= require vendor/instantclick.min
 
 $(function() {
   var nav = responsiveNav(".nav-collapse");
