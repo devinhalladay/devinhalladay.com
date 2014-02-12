a0417baa516efd7c934f210efac716d086d9f081
Devin Halladay <devin@devinhalladay.com>
2014-02-11 20:21:40 -0500

    Removed responsive-nav and improved nav at mobile breakpoints. Left-aligned large screen nav

--- _assets/js/stuff.js
+++ _assets/js/stuff.js
@@ -1,9 +1,9 @@
-//= require vendor/responsive-nav.min
+// require vendor/responsive-nav.min
 //= require vendor/ss-standard
 //= require vendor/wufoo
 //= require vendor/parsley.min
 //= require vendor/parsley.extend
 
-$(function() {
+/*$(function() {
   var nav = responsiveNav(".nav-collapse");
-});
+});*/
