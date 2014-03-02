d3ea85e8c4c6361f4404a05d37f0526356e6a67a
Devin Halladay <devin@devinhalladay.com>
2014-03-01 21:45:54 -0500

    Removing unnecessary requires from stuff.js

--- _assets/js/stuff.js
+++ _assets/js/stuff.js
@@ -1,14 +1,8 @@
-// require vendor/responsive-nav.min
-//= require vendor/ss-standard
 //= require vendor/wufoo
 //= require vendor/parsley.min
 //= require vendor/parsley.extend
-//= require vendor/jquery.fluidbox.min
 //= require vendor/imagesloaded.pkgd.min
-
-/*$(function() {
-  var nav = responsiveNav(".nav-collapse");
-});*/
+//= require vendor/jquery.fluidbox.min
 
 $(function () {
   $("li figure a").fluidbox();
