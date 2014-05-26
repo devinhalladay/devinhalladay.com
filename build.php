fae943dcc21f38cc7268cbb66d897d312b09fa32
Devin Halladay <devin@devinhalladay.com>
2014-05-26 15:34:16 -0400

    Fixing build scripts.

--- build.php
+++ build.php
@@ -3,6 +3,5 @@
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 
 shell_exec( 'cd /var/www/devinhalladay.com/public_html/ && git fetch github master' );
-shell_exec( '/usr/local/rvm/gems/ruby-2.1.1/gems/jekyll-1.4.3/bin/jekyll' );
 
 ?>
