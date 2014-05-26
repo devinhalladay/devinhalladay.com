f760309ab70874dca1c5cc0aaac45f374bdfa141
Devin Halladay <devin@devinhalladay.com>
2014-05-26 14:44:54 -0400

    Updating build.php

--- build.php
+++ build.php
@@ -3,6 +3,5 @@
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 
 shell_exec( 'cd /var/www/devinhalladay.com/public_html/ && git fetch github master' );
-shell_exec( 'jekyll build' );
 
 ?>
