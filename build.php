f6ad5bc4f883a421c77ee168d272156fc0ad1fc2
Devin Halladay <devin@devinhalladay.com>
2014-05-26 14:22:13 -0400

    Removed travis.yml and updated build.php to fetch instead of pull.

--- build.php
+++ build.php
@@ -2,7 +2,7 @@
 
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 
-shell_exec( 'cd /var/www/devinhalladay.com/public_html/ && git pull github master' );
+shell_exec( 'cd /var/www/devinhalladay.com/public_html/ && git fetch github master' );
 shell_exec( 'jekyll build' );
 
 ?>
