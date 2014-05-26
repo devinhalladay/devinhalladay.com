19f5fdd82f0aa752501ddf099852d3f5c4a2d6fa
Devin Halladay <devin@devinhalladay.com>
2014-05-26 13:19:33 -0400

    Fuck webhooks.

--- build.php
+++ build.php
@@ -3,7 +3,7 @@
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 
 if ( $_POST['payload'] ) {
-  shell_exec( 'cd /var/www/devinhalladay.com/public_html/ && git pull' );
+  shell_exec( 'cd /var/www/devinhalladay.com/public_html/ && git pull github master' );
   shell_exec( 'jekyll build' );
 }
 
