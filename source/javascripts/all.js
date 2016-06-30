f2b82e5caa0f0c345c582ac3004b9529f5b793c5
Devin Halladay <devin@devinhalladay.com>
2016-06-29 22:26:46 -0400

    Add esc keypress to close fullscreen nav

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -5,3 +5,10 @@ $( document ).ready(function() {
     $('#overlay').toggleClass('open');
   })
 });
+
+$(document).keyup(function(e) {
+  if (e.keyCode == 27) { // escape key maps to keycode `27`
+    $('#toggle').removeClass('active');
+    $('#overlay').removeClass('open');
+  }
+});
