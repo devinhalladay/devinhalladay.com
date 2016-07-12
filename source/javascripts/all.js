a5489d3c4a2fa359efa8e08805b83c155589144a
Devin Halladay <devin@devinhalladay.com>
2016-07-12 18:34:21 -0400

    Got AJAX form submission and validation working

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -12,3 +12,54 @@ $(document).keyup(function(e) {
     $('#overlay').removeClass('open');
   }
 });
+
+$(document).ready(function() {
+  // process the form
+  $('form').submit(function(event) {
+
+    $('#name').removeClass('has-error'); // remove the error class
+    $('#email').removeClass('has-error'); // remove the error class
+    $('#message').removeClass('has-error'); // remove the error class
+
+    // get the form data
+    // there are many ways to get this data using jQuery (you can use the class or id also)
+    var formData = $(this).serialize();
+    // process the form
+    $.ajax({
+      type        : 'POST',
+      url         : 'https://devinhalladay-contact.herokuapp.com', // the url where we want to POST
+      data        : formData, // our data object
+      dataType    : 'json', // what type of data do we expect back from the server
+      encode          : true
+    })
+    // using the done promise callback
+    .done(function(data) {
+
+      // log data to the console so we can see
+      console.log(data);
+
+      // handle errors and validation messages
+      if ( ! data.success) {
+
+        // handle name errors
+        if (data.errors.name) {
+          $('#name').addClass('has-error');
+        }
+
+        // handle email errors
+        if (data.errors.email) {
+          $('#email').addClass('has-error');
+        }
+
+        // message email errors
+        if (data.errors.message) {
+          $('#message').addClass('has-error');
+        }
+      } else {
+        alert('success');
+      }
+    });
+    // stop the form from submitting the normal way and refreshing the page
+    event.preventDefault();
+  });
+});
