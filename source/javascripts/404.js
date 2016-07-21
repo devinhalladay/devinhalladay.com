67aabf89e0982345233f2fe1d1c78d2c81133101
Devin Halladay <devin@devinhalladay.com>
2016-07-21 17:07:56 -0400

    Added 404 page

---
+++ source/javascripts/404.js
@@ -0,0 +1,11 @@
+//= require vendor/typed
+
+// Typed function for the 404 page.
+$(document).ready(function(){
+  $(function(){
+    $(".console").typed({
+      strings: ["4 ^600 8 ^600 15 ^600 16 ^600 23 ^600 42 "],
+      typeSpeed: 0
+    });
+  });
+});
