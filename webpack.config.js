221c81d930e7832e231527d38cc1677454bf18c1
Devin Halladay <devin@devinhalladay.com>
2016-08-18 03:52:51 -0400

    Optimizing images, working on case studies

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,13 @@ module.exports = {
           presets: ['es2015', 'stage-0'],
         },
       },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+            'file?hash=sha512&digest=hex&name=[hash].[ext]',
+            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
+      }
     ],
   },
 
